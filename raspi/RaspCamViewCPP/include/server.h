0bc215bd69a67f21ef0a280b81554b7b0c419650
Raffaello <raffaello.salvetti@gmail.com>
2019-10-15 23:41:01 -0300

    adicionado modelo do projeto; adicionado programa de controle de camera usando servoblaster

---
+++ raspi/RaspCamViewCPP/include/server.h
@@ -0,0 +1,6 @@
+#ifndef __SERVER_H_
+#define __SERVER_H_
+
+#define SERVER_PORT 3200
+
+#endif
---
+++ raspi/RaspCamViewCPP/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "include/server.h"
+
+int main() {
+  printf("Hell World!!\n");
+}
