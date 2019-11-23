8db67b5acf4489d086d964dfb25fe57ae8a0814a
Raffaello <raffaello.salvetti@gmail.com>
2019-11-23 14:22:13 -0300

    adicionado funcao de calibracao de camera; ajuste em conclusao

--- raspi/RaspCamViewCPP/include/servo.h
+++ raspi/RaspCamViewCPP/include/servo.h
@@ -4,9 +4,15 @@
 #define SERVO_X_GPIO     4
 #define SERVO_Y_GPIO     1
 
+typedef struct t_center {
+    int x;
+    int y;
+} t_center;
+
 int servo_driver_init();
 void servo_driver_set_x_position(int x);
 void servo_driver_set_y_position(int y);
+void servo_driver_calibrate();
 void servo_driver_destroy();
 
 #endif
